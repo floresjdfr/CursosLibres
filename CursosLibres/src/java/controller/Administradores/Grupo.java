4ed8e650c4909af451d37c5f6e49c03ef215f68f
Rando97 <boyrojas97@gmail.com>
2021-04-29 14:13:17 -0600

    al carajo la vida de uevo
    prueba 2

--- CursosLibres/src/java/controller/Administradores/Grupo.java
+++ CursosLibres/src/java/controller/Administradores/Grupo.java
@@ -25,10 +25,13 @@ import logic.usuario.Usuario;
  * @author flore
  */
 @WebServlet(name = "Grupo", urlPatterns = {"/ListarGrupos"})
+@WebServlet(name = "Grupo", urlPatterns = {"/ListarGrupos", "/agregarGrupo", "editarGrupo"})
 public class Grupo extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
      *
      * @param request servlet request
      * @param response servlet response
@@ -39,15 +42,23 @@ public class Grupo extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         String URL = "";
-        
-        switch(request.getServletPath()){
-            case "/ListarGrupos":{
+
+        switch (request.getServletPath()) {
+            case "/ListarGrupos": {
                 URL = listarGrupos(request);
+                break;
+            }
+            case "/agregarGrupo": {
+                URL = agregarGrupo(request);
+                break;
+            }
+            case "/editarGrupo": {
+                URL = editarGrupo(request);
                 break;
             }
         }
         request.getRequestDispatcher(URL).forward(request, response);
-        
+
         
     }
 
@@ -100,18 +111,66 @@ public class Grupo extends HttpServlet {
             } catch (IOException | URISyntaxException | SQLException ex) {
                 Logger.getLogger(Grupo.class.getName()).log(Level.SEVERE, null, ex);
             }
+        if (validarAdmin(request)) {
+            String idCursoString = request.getParameter("idCurso");
+            Service listaGrupos = GrupoDAO.obtenerInstancia().listarGrupos(idCursoString);
+            request.setAttribute("listaGrupos", listaGrupos);
+            return "/presentation/misc/Grupos.jsp";
         }
         return "/Cursos";
     }
-    
-    private Boolean validarAdmin(HttpServletRequest request){
+
+    private String agregarGrupo(HttpServletRequest request) {
+
+        try {
+
+            if (validarAdmin(request)) {
+                logic.grupo.Grupo g = new logic.grupo.Grupo();
+                g.setCurso_codigo(Integer.parseInt(request.getParameter("idCurso")));
+                g.setProfesor_idPreofesor(Integer.parseInt(request.getParameter("idProfesor")));
+                g.setFecha(request.getParameter("fecha"));
+                GrupoDAO.obtenerInstancia().crearGrupo(g);
+
+                return "/presentation/misc/Grupos.jsp";
+
+            }
+        } catch (Exception e) {
+
+        }
+        return "/Cursos";
+    }
+
+    private String editarGrupo(HttpServletRequest request) {
+        try {
+            if (validarAdmin(request)) {
+
+                logic.grupo.Grupo g = new logic.grupo.Grupo();
+                g.setCodigo(Integer.parseInt(request.getParameter("idGrupo")));
+                g.setCurso_codigo(Integer.parseInt(request.getParameter("idCurso")));
+                g.setProfesor_idPreofesor(Integer.parseInt(request.getParameter("idProfesor")));
+                g.setFecha(request.getParameter("fecha"));
+                GrupoDAO.obtenerInstancia().crearGrupo(g);
+
+                return "/presentation/misc/Grupos.jsp";
+            }
+        } catch (Exception e) {
+
+        }
+        return "/Cursos";
+    }
+
+    private Boolean validarAdmin(HttpServletRequest request) {
         HttpSession session = request.getSession();
         Usuario usr = (Usuario) session.getAttribute("usr");
         if (usr != null)
             if (usr.getClass().getSimpleName().equals("Administrador"))
+        if (usr != null) {
+            if (usr.getClass().getSimpleName().equals("Administrador")) {
                 return true;
+            }
+        }
         return false;
-        
+
     }
 
 }
